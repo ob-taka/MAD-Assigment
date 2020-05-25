361314fb2528d2309f52b1a1d8495fbfc25be393
ob-taka <xiongrunlinxiong@gmail.com>
2020-05-25 17:03:44 +0800

    mic

--- app/src/main/java/com/example/mad_assigment/test.java
+++ app/src/main/java/com/example/mad_assigment/test.java
@@ -2,7 +2,10 @@ package com.example.mad_assigment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class test extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class test extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+        Button button = findViewById(R.id.button);
+        Button button2 = findViewById(R.id.button2);
+
+
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), User_home.class);
+                startActivity(intent);
+            }
+        });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), DoctorHome.class);
+                startActivity(intent);
+            }
+        });
     }
 }
