c33598b1739ae80e9d597237f60ebd2978d44499
Primaryheap <s10185319@connect.np.edu.sg>
2020-05-25 17:18:08 +0800

    added onclicklistener

--- app/src/main/java/com/example/mad_assigment/DoctorHome.java
+++ app/src/main/java/com/example/mad_assigment/DoctorHome.java
@@ -1,14 +1,41 @@
 package com.example.mad_assigment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 public class DoctorHome extends AppCompatActivity {
+
+    Button viewpatient;
+    Button addpatient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.doctor_home);
+
+        viewpatient = findViewById(R.id.button4);
+        addpatient = findViewById(R.id.button5);
+
+        viewpatient.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), PatientList.class);
+                startActivity(intent);
+            }
+        });
+
+        addpatient.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), AddPatient.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
