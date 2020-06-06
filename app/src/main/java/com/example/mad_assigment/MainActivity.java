4484aacbd108cde6bf073b7e556f5eea7e7e9e80
Primaryheap <s10185319@connect.np.edu.sg>
2020-06-06 12:47:05 +0800

    fixed merge bugs:

--- app/src/main/java/com/example/mad_assigment/MainActivity.java
+++ app/src/main/java/com/example/mad_assigment/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getSupportActionBar().hide();
+        //getSupportActionBar().hide();
         //this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         Button button = findViewById(R.id.button);
