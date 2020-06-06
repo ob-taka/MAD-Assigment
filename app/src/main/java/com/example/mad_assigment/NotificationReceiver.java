3e8fc9023afe6a4e515379aa3e5a0d519fb64016
ob-taka <xiongrunlinxiong@gmail.com>
2020-06-06 20:39:09 +0800

    degugs

---
+++ app/src/main/java/com/example/mad_assigment/NotificationReceiver.java
@@ -0,0 +1,23 @@
+package com.example.mad_assigment;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+public class NotificationReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        NotificationCompat.Builder nb = new NotificationCompat.Builder(context, "channelID")
+                .setSmallIcon(R.drawable.pill)
+                .setContentTitle("Title")
+                .setContentText("Text")
+                .setPriority(NotificationCompat.PRIORITY_HIGH);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+
+        notificationManager.notify(100, nb.build());
+    }
+}
--- app/src/main/java/com/example/mad_assigment/User_home.java
+++ app/src/main/java/com/example/mad_assigment/User_home.java
@@ -1,27 +1,40 @@
 package com.example.mad_assigment;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.util.Log;
 import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
+import java.text.DateFormat;
 import java.util.ArrayList;
-import java.util.Queue;
+import java.util.Calendar;
 
 public class User_home extends AppCompatActivity {
 
-    private FirebaseDatabase db = FirebaseDatabase.getInstance();
     private MAdaptor adaptor;
+    private final DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("user_medicien");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,9 +42,12 @@ public class User_home extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_user_home);
+        createChannel();
 
         setUpRecyclerView();
+
     }
+
 
     private void setUpRecyclerView(){
         Query query = FirebaseDatabase.getInstance().getReference().child("user_medicien").orderByChild("priority");
@@ -48,13 +64,23 @@ public class User_home extends AppCompatActivity {
         adaptor.setOnItemClickListener(new MAdaptor.OnItemClickListener() {
             @Override
             public void onItemClick(DataSnapshot dataSnapshot, int position) {
-                String id = dataSnapshot.getKey();
                 Modle modle = adaptor.getItem(position);
                 modle.setExpanded(!modle.isExpanded());
                 adaptor.notifyItemChanged(position);
-                Toast.makeText(User_home.this, "ID: " + id, Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+
+
+    private void createChannel(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel("channelID", "reminderChannel", NotificationManager.IMPORTANCE_HIGH);
+            channel.setDescription("channel for reminder");
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 
     @Override
