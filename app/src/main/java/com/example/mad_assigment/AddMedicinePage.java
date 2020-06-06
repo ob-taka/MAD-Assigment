e6eb0b124136c3b61edff821c38af6800a23e686
Shrey Chauhan <s10197306@connect.np.edu.sg>
2020-06-06 16:19:29 +0800

    Add medicine(error)

--- app/src/main/java/com/example/mad_assigment/AddMedicinePage.java
+++ app/src/main/java/com/example/mad_assigment/AddMedicinePage.java
@@ -1,21 +1,31 @@
 package com.example.mad_assigment;
 
+import android.app.Activity;
+import android.app.SearchManager;
 import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.InputType;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.RadioGroup;
+import android.widget.SearchView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -24,12 +34,14 @@ import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.lang.reflect.Array;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 
@@ -41,7 +53,9 @@ public class AddMedicinePage extends AppCompatActivity {
     ImageButton plus,minus;
     Button breakfast,lunch,dinner,submit;
     Double doseNumber;
+
     Integer correctMed,breakfastValid,lunchValid,dinnerValid;
+
 
     DatabaseReference databaseReference;
     ArrayList<String> med_list;
@@ -125,8 +139,8 @@ public class AddMedicinePage extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if(breakfastValid==0){
-                breakfastValid=1;
-                breakfast.setBackground(getResources().getDrawable(R.drawable.btn_selected));
+                    breakfastValid=1;
+                    breakfast.setBackground(getResources().getDrawable(R.drawable.btn_selected));
                 }
                 else{
                     breakfastValid=0;
@@ -139,8 +153,8 @@ public class AddMedicinePage extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if(lunchValid==0){
-                lunchValid=1;
-                lunch.setBackground(getResources().getDrawable(R.drawable.btn_selected));
+                    lunchValid=1;
+                    lunch.setBackground(getResources().getDrawable(R.drawable.btn_selected));
                 }
                 else{
                     lunchValid=0;
@@ -153,8 +167,8 @@ public class AddMedicinePage extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if(dinnerValid==0){
-                dinnerValid=1;
-                dinner.setBackground(getResources().getDrawable(R.drawable.btn_selected));
+                    dinnerValid=1;
+                    dinner.setBackground(getResources().getDrawable(R.drawable.btn_selected));
                 }
                 else{
                     dinnerValid=0;
@@ -168,9 +182,9 @@ public class AddMedicinePage extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if(medType.equals("tablet")){
-                doseNumber++;
-                String doseString= df.format(doseNumber);
-                dose.setText(doseString);
+                    doseNumber++;
+                    String doseString= df.format(doseNumber);
+                    dose.setText(doseString);
                 }
                 else if (medType.equals("liquid"))
                 {
@@ -241,7 +255,7 @@ public class AddMedicinePage extends AppCompatActivity {
                 if (!s.toString().isEmpty()) {
                     medName=searchMed.getText().toString();
                     if(searchMed.hasFocus()){
-                    setAdapter(s.toString());}
+                        setAdapter(s.toString());}
 
                 }
 
@@ -311,23 +325,31 @@ public class AddMedicinePage extends AppCompatActivity {
         databaseReference.child("Medicine").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
                     String med_name=snapshot.child("medicineTitle").getValue().toString();
                     Log.v("number",correctMed.toString());
                     if (med_name.equals(medName)) {
                         correctMed=1;
+
                     }
+
                 }
                 if (correctMed==0){
                     searchMed.setError("Invalid Medicine Name");
                 }
+
             }
+
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
         });
         String errorMsg = "";
+
+
+
 
         if (radioGroup.getCheckedRadioButtonId() == -1) {
             errorMsg+="Please select before or after food";
@@ -339,9 +361,11 @@ public class AddMedicinePage extends AppCompatActivity {
             errorMsg+="\n Please select the dosage";
         }
 
-            Toast toast = Toast.makeText(getApplicationContext(),
-                    errorMsg,
-                    Toast.LENGTH_LONG);
+
+
+        Toast toast = Toast.makeText(getApplicationContext(),
+                errorMsg,
+                Toast.LENGTH_LONG);
 
         toast.show();
 
