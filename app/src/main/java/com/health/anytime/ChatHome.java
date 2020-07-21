f7647d729f9715d28534f0d5329c599037228b13
Dong Yuan Kester Yeo <s10185261@connect.np.edu.sg>
2020-07-22 02:15:30 +0800

    First progress on Chat Implementation. -Currently constructing the main layout for the UI. -Completed tab layout and view pager adapter. -Tabs such as "Chats" and "Contacts" is now clickable

---
+++ app/src/main/java/com/health/anytime/ChatHome.java
@@ -0,0 +1,36 @@
+package com.health.anytime;
+
+import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
+
+public class ChatHome extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private ViewPager mViewPager;
+    private TabLayout mTabLayout;
+    private TabsAccessorAdapter mTAA;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_main);
+
+        mToolbar = findViewById(R.id.main_chat_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Chat");
+
+        mViewPager = findViewById(R.id.chat_ViewPager);
+        mTAA = new TabsAccessorAdapter(getSupportFragmentManager(),TabsAccessorAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        mViewPager.setAdapter(mTAA);
+
+        mTabLayout = findViewById(R.id.chat_tab);
+        mTabLayout.setupWithViewPager(mViewPager);
+    }
+}
