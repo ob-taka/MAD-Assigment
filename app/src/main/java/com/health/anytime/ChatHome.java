2c43630ebcce23c9132e27e86cd4f2c97402254a
Dong Yuan Kester Yeo <s10185261@connect.np.edu.sg>
2020-07-22 12:13:40 +0800

    More progress on Chat Implementation. -Attempting to get a list of contacts from firebase database but in order to do that, I will need to merge signin and signup branches and implement sharedpreferences.

--- app/src/main/java/com/health/anytime/ChatHome.java
+++ app/src/main/java/com/health/anytime/ChatHome.java
@@ -21,16 +21,18 @@ public class ChatHome extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_main);
-
+        //Setting of the toolbar as well as the title
         mToolbar = findViewById(R.id.main_chat_toolbar);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle("Chat");
 
+        //The following below allows the user to switch between "Chat" and "Doctors"
         mViewPager = findViewById(R.id.chat_ViewPager);
         mTAA = new TabsAccessorAdapter(getSupportFragmentManager(),TabsAccessorAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
         mViewPager.setAdapter(mTAA);
 
         mTabLayout = findViewById(R.id.chat_tab);
         mTabLayout.setupWithViewPager(mViewPager);
+
     }
 }
