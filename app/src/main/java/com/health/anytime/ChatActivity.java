686044181c14394daa99621decca30413119d750
Dong Yuan Kester Yeo <s10185261@connect.np.edu.sg>
2020-07-26 02:10:20 +0800

    able to click on user in contacts to open chat activity.. more to be completed

---
+++ app/src/main/java/com/health/anytime/ChatActivity.java
@@ -0,0 +1,13 @@
+package com.health.anytime;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
